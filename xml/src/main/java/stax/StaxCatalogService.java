c9d4439facb8df14f9a713894ecb662b3c316bfc
vicziani <viczian.istvan@gmail.com>
2019-03-04 16:45:14 +0100

    STAX írás

--- xml/src/main/java/stax/StaxCatalogService.java
+++ xml/src/main/java/stax/StaxCatalogService.java
@@ -5,6 +5,7 @@ import javax.xml.stream.*;
 import javax.xml.stream.events.StartElement;
 import javax.xml.stream.events.XMLEvent;
 import java.io.InputStream;
+import java.io.StringWriter;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -64,5 +65,29 @@ public class StaxCatalogService {
         catch (XMLStreamException e) {
             throw new IllegalStateException("Can not parse xml", e);
         }
+    }
+
+    public String writeToXml(List<Book> books) {
+        try {
+            XMLOutputFactory output = XMLOutputFactory.newInstance();
+            StringWriter stringWriter = new StringWriter();
+            XMLStreamWriter streamWriter = output.createXMLStreamWriter(stringWriter);
+            streamWriter.writeStartDocument();
+            streamWriter.writeStartElement("catalog");
+            for (Book book: books) {
+                streamWriter.writeStartElement("book");
+                streamWriter.writeAttribute("isbn10", book.getIsbn10());
+                streamWriter.writeStartElement("title");
+                streamWriter.writeCharacters(book.getTitle());
+                streamWriter.writeEndElement();
+                streamWriter.writeEndElement();
+            }
+            streamWriter.writeEndElement();
+            streamWriter.flush();
+            return streamWriter.toString();
+        } catch (XMLStreamException xse) {
+            throw new IllegalStateException("Error writing xml", xse);
+        }
+
     }
 }
