bb3354ae8ce84bb03a4aa384a086a12f6ab61900
vicziani <viczian.istvan@gmail.com>
2019-03-05 16:06:03 +0100

    JAXB main

---
+++ catalog-jaxb/src/main/java/catalog/MarshallMain.java
@@ -0,0 +1,24 @@
+package catalog;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import java.util.List;
+
+public class MarshallMain {
+
+    public static void main(String[] args) throws JAXBException {
+        JAXBContext ctx = JAXBContext.newInstance(Catalog.class, Book.class);
+        Marshaller marshaller = ctx.createMarshaller();
+
+        Catalog catalog = new Catalog(List.of(
+                new Book("Java and XML", "x111", 1999),
+                new Book("Java and JAXB", "x112", 2011),
+                new Book("Java and Java", "x113", 2010),
+                new Book("Java and Spring", "x114", 2010)
+        ));
+
+        marshaller.marshal(catalog, System.out);
+
+    }
+}
