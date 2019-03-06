0e6acb5d1f5f9f71be75b8b558cf577125f1447b
vicziani <viczian.istvan@gmail.com>
2019-03-06 13:40:25 +0100

    Személyreszabás

--- books-service/src/main/java/catalog/BookService.java
+++ books-service/src/main/java/catalog/BookService.java
@@ -1,13 +1,19 @@
 package catalog;
 
 import javax.jws.WebMethod;
+import javax.jws.WebResult;
 import javax.jws.WebService;
+import javax.xml.ws.RequestWrapper;
+import javax.xml.ws.ResponseWrapper;
 import java.util.List;
 
 @WebService
 public class BookService {
 
     @WebMethod
+    @WebResult(name = "book")
+    @ResponseWrapper(localName = "catalog")
+    @RequestWrapper(localName = "list-catalog")
     public List<Book> findAllBooks() {
         return List.of(
                 new Book("Java and SOAP", "x1234"),
