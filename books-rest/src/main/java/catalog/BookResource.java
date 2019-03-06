2c59006bba3bb0d3c4699f62b39ac03d6069027a
vicziani <viczian.istvan@gmail.com>
2019-03-06 15:35:55 +0100

    findbyisbn10

--- books-rest/src/main/java/catalog/BookResource.java
+++ books-rest/src/main/java/catalog/BookResource.java
@@ -1,8 +1,13 @@
 package catalog;
 
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import java.util.ArrayList;
 import java.util.List;
 
+@Path("books")
 public class BookResource {
 
     private static final List<Book> books =
@@ -11,11 +16,20 @@ public class BookResource {
                     new Book("Java and XML", "x2")
             ));
 
+    @GET
     public List<Book> listBooks() {
         return books;
     }
 
+    @POST
     public void addBook(Book book) {
         books.add(book);
     }
+
+    @Path("{isbn10}")
+    public Book findBookByIsbn10(@PathParam("isbn10") String isbn10) {
+        return books.stream().filter(b -> b.getIsbn10().equals(isbn10)).findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("Book with isdb10 not found"));
+    }
+
 }
